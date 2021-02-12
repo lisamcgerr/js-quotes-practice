28301a329c1487d529da9cb56e69ba5e50542268
Lisa Marie <73184313+lisamcgerr@users.noreply.github.com>
2021-02-12 07:42:34 -0600

    Update index.js
    done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 function main(){
     fetchQuotes()
     createFormListener()
-    createDeleteListener()
     createLikeListener()
+    deleteQuote()
 }
+
 
 function fetchQuotes(){
     fetch('http://localhost:3000/quotes?_embed=likes')
@@ -16,10 +17,9 @@ function fetchQuotes(){
     })
 }
 
-
 function renderQuotes(quote){
     const ul = document.querySelector('#quote-list')
-
+    
     const li = document.createElement('li')
     li.className = 'quote-card'
 
@@ -27,7 +27,7 @@ function renderQuotes(quote){
     blockQuote.className = 'blockquote'
 
     const pTag = document.createElement('p')
-    pTag.className = `mb-0`
+    pTag.className = 'mb-0'
     pTag.innerText = quote.quote
 
     const footer = document.createElement('footer')
@@ -38,49 +38,113 @@ function renderQuotes(quote){
 
     const likeBtn = document.createElement('button')
     likeBtn.className = 'btn-success'
-    likeBtn.innerText = `Likes:`
+    likeBtn.innerText = 'Likes: '
+    likeBtn.dataset.id = quote.id
 
     const span = document.createElement('span')
     span.innerText = quote.likes.length
 
     const deleteBtn = document.createElement('button')
     deleteBtn.className = 'btn-danger'
-    deleteBtn.innerText = 'delete'
+    deleteBtn.innerText = 'Delete'
     deleteBtn.dataset.id = quote.id
 
     ul.append(li)
-    likeBtn.append(span)
     li.append(blockQuote)
     blockQuote.append(pTag, footer, br, likeBtn, deleteBtn)
+    likeBtn.append(span)
 }
 
 
 function createFormListener(){
+    const form = document.querySelector('#new-quote-form')
+    form.addEventListener('submit', function(e){
+        e.preventDefault()
+
+        const newQuote = {
+            quote: e.target.quote.value,
+            author: e.target.author.value
+        }
+
+        const reqObj = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newQuote)
+        }
+
+        fetch('http://localhost:3000/quotes?_embed=likes', reqObj)
+        .then(resp => resp.json())
+        .then(quote => {
+            e.target.reset()
+
+            const updatedQuote = {
+                ...quote,
+                likes: []
+            }
+            renderQuotes(updatedQuote)
+        })
+    })
 
 }
 
-function createDeleteListener(){
-    const ul = document.querySelector('#quote-list')
-    ul.addEventListener('click', function(e){
-        if (e.target.className === 'btn-danger'){
-
-            const id = e.target.dataset.id
-
-            const reqObj = {
-                method: 'DELETE'
-            }
-
-            fetch(`http://localhost:3000/quotes/${id}`, reqObj)
-            .then (resp => resp.json())
-            .then (quote => {
-                console.log(e.target.parentNode)
-            })
+function createLikeListener(e){
+    const quoteContainer = document.querySelector('#quote-list')
+    quoteContainer.addEventListener('click', function(e){
+        if (e.target.className === 'btn-success'){
+            likeQuote(e)
         }
+
     })
 }
 
-function createLikeListener(){
+function likeQuote(e){
+    console.log(e.target)
+    const id = e.target.dataset.id
+    const span = e.target.firstElementChild
+    let likes = parseInt(span.innerText)
+
+    const reqObj = {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({likes: likes + 1})
+    }
+
+    fetch(`http://localhost:3000/quotes/${id}`, reqObj)
+    .then(resp => resp.json())
+    .then(quote => {
+        span.innerText = `${likes + 1}`
+    })
 
 }
+
+function deleteQuote(){
+    const quoteContainer = document.querySelector('#quote-list')
+    quoteContainer.addEventListener('click', function(e){
+        if (e.target.className === 'btn-danger'){
+            deleteQ(e)
+        }
+
+    })
+}
+
+function deleteQ(e){
+
+    const id = e.target.dataset.id
+    const reqObj = {
+        method: 'DELETE'
+    }
+
+    fetch(`http://localhost:3000/quotes/${id}`, reqObj)
+    .then(resp => resp.json())
+    .then(quote => {
+        e.target.parentNode.parentNode.remove()
+    })
+}
+
+
 
 main()
